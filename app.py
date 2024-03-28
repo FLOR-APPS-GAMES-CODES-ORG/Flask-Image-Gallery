3fd6f8f64c5d83a875159a2b9645840df085ed72
= <=>
2024-03-28 19:17:00 +0100

    return data

--- app.py
+++ app.py
@@ -7,6 +7,27 @@ import glob
 import sys
 import binascii
 import argparse
+import openpyxl
+import pandas as pd
+import json
+sheet=None
+
+def get_sheet(excl):
+    xls = pd.ExcelFile(excl)
+    sheet=xls.sheet_names[3]
+
+    return sheet
+
+def get_word_data(word):
+    sheetx=sheet
+    excl="word_story_image.xlsx"
+    if(sheetx==None):
+       sheetx= get_sheet(excl)
+    df = pd.read_excel(excl, sheet_name=sheetx)
+    wordmatches=df[df["Dutch"]==str(word)].to_json(orient="columns")
+    return str(wordmatches)+"ik,i,story  asdfaf,183b82ec6cf0c5d603fd19bdf31c9e062aad7b87.png"
+
+
 app = Flask("Flask Image Gallery")
 app.config['IMAGE_EXTS'] = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
 def encode(x):
@@ -28,12 +49,13 @@ def download_file(filepath):
     dir,filename = os.path.split(decode(filepath))
     return send_from_directory(dir, filename, as_attachment=False)
 
-@app.route('/cdn/<word:word>')
+@app.route('/worddata/<word>')
 def getWordData(word):
-    word_data = "ik,i,storasdfaf,183b82ec6cf0c5d603fd19bdf31c9e062aad7b87.png" 
-    return send_from_directory(word_data, as_attachment=False)
+    word_data = get_word_data(word) 
+    return  word_data # send_from_directory(word_data, as_attachment=False)
 
 if __name__=="__main__":
+    get_word_data("hebben")
     parser = argparse.ArgumentParser('Usage: %prog [options]')
     #parser.add_argument('root_dir', help='Gallery root directory path')
     parser.add_argument('-l', '--listen', dest='host', default='127.0.0.1', \

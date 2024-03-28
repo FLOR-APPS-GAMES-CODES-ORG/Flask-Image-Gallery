611cb265caed00ff00c32afddf5edf0c3f8f3e89
Arif YILMAZ <73948166+arifx@users.noreply.github.com>
2024-03-28 15:06:54 +0100

    customize for vocab

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -8,29 +7,21 @@ import glob
 import sys
 import binascii
 import argparse
-
-
 app = Flask("Flask Image Gallery")
 app.config['IMAGE_EXTS'] = [".png", ".jpg", ".jpeg", ".gif", ".tiff"]
-
-
 def encode(x):
-    return binascii.hexlify(x.encode('utf-8')).decode()
-
+    return  x #binascii.hexlify(x.encode('utf-8')).decode()
 def decode(x):
-    return binascii.unhexlify(x.encode('utf-8')).decode()
-
-
-@app.route('/')
+    return x #binascii.unhexlify(x.encode('utf-8')).decode()
+@app.route('/noroot')
 def home():
-    root_dir = app.config['ROOT_DIR']
+    root_dir =  "assets" #app.config['ROOT_DIR']
     image_paths = []
     for root,dirs,files in os.walk(root_dir):
         for file in files:
             if any(file.endswith(ext) for ext in app.config['IMAGE_EXTS']):
                 image_paths.append(encode(os.path.join(root,file)))
     return render_template('index.html', paths=image_paths)
-
 
 @app.route('/cdn/<path:filepath>')
 def download_file(filepath):
@@ -40,11 +31,12 @@ def download_file(filepath):
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser('Usage: %prog [options]')
-    parser.add_argument('root_dir', help='Gallery root directory path')
+    #parser.add_argument('root_dir', help='Gallery root directory path')
     parser.add_argument('-l', '--listen', dest='host', default='127.0.0.1', \
                                     help='address to listen on [127.0.0.1]')
     parser.add_argument('-p', '--port', metavar='PORT', dest='port', type=int, \
                                 default=5000, help='port to listen on [5000]')
     args = parser.parse_args()
-    app.config['ROOT_DIR'] = args.root_dir
+    #app.config['ROOT_DIR'] = args.root_dir
     app.run(host=args.host, port=args.port, debug=True)
+    
